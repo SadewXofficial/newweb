548574a9b43a54acdd32d855fd261ad889610e5d
SadewXofficial <160341086+SadewXofficial@users.noreply.github.com>
2026-05-24 11:48:40 +0530

    Add files via upload

---
+++ js/Canvas.astro_astro_type_script_index_0_lang.BlBf87nn.js
@@ -0,0 +1,1 @@
+import{C as s}from"./Canvas.Bo70JWoD.js?dpl=dpl_GaHoUqyxSeox4Bq81citwmC7er5b";customElements.get("c-canvas")||customElements.define("c-canvas",s);
---
+++ js/Loader.astro_astro_type_script_index_0_lang.BhCbSigP.js
@@ -0,0 +1,1 @@
+import{L as e}from"./Loader.BSMhjkJy.js?dpl=dpl_GaHoUqyxSeox4Bq81citwmC7er5b";customElements.get("c-loader")||customElements.define("c-loader",e);
