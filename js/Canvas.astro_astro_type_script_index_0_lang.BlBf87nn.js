da3b63f0ba9eec3aa5cfd65b9c4368ac4bcc89d7
SadewXofficial <160341086+SadewXofficial@users.noreply.github.com>
2026-05-24 12:48:39 +0530

    Delete js directory

--- js/Canvas.astro_astro_type_script_index_0_lang.BlBf87nn.js
+++
@@ -1,1 +0,0 @@
-import{C as s}from"./Canvas.Bo70JWoD.js?dpl=dpl_GaHoUqyxSeox4Bq81citwmC7er5b";customElements.get("c-canvas")||customElements.define("c-canvas",s);
--- js/Loader.astro_astro_type_script_index_0_lang.BhCbSigP.js
+++
@@ -1,1 +0,0 @@
-import{L as e}from"./Loader.BSMhjkJy.js?dpl=dpl_GaHoUqyxSeox4Bq81citwmC7er5b";customElements.get("c-loader")||customElements.define("c-loader",e);
